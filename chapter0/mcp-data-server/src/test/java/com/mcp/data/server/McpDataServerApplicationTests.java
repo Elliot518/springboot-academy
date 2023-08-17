309eed40a24f45ab41d29799f7b0373325e37c2d
Elliot <elliotgjy@163.com>
2023-08-17 22:14:26 +0800

    create skeleton from spring boot initializr

---
+++ chapter0/mcp-data-server/src/test/java/com/mcp/data/server/McpDataServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcp.data.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpDataServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
