ae0cefa79cc0552688e5e2d2d168b05bd8d58d53
Elliot <elliotgjy@163.com>
2023-09-25 10:52:52 +0800

    first app

---
+++ SpringBoot3/chapter1/first-app/src/test/java/com/mcp/springboot/learn/FirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcp.springboot.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
