919a060c43ceb56e95ddcae05270b47d8d4a26d5
Elliot <elliotgjy@163.com>
2023-12-12 17:21:13 +0800

    update

--- SpringBoot3/spring-security-basic/src/main/java/com/packt/cardatabase/SecurityConfig.java
+++ SpringBoot3/spring-security-basic/src/main/java/com/packt/cardatabase/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -26,14 +27,9 @@ import com.packt.cardatabase.service.UserDetailsServiceImpl;
 @EnableWebSecurity
 public class SecurityConfig {
 	private final UserDetailsServiceImpl userDetailsService;
-	private final AuthenticationFilter authenticationFilter;
-	private final AuthEntryPoint exceptionHandler;
 
-	public SecurityConfig(UserDetailsServiceImpl userDetailsService, AuthenticationFilter authenticationFilter,
-			AuthEntryPoint exceptionHandler) {
+	public SecurityConfig(UserDetailsServiceImpl userDetailsService) {
 		this.userDetailsService = userDetailsService;
-		this.authenticationFilter = authenticationFilter;
-		this.exceptionHandler = exceptionHandler;
 	}
 
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
@@ -41,37 +37,7 @@ public class SecurityConfig {
 	}
 
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.csrf((csrf) -> csrf.disable()).cors(withDefaults())
-				.sessionManagement(
-						(sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
-						.requestMatchers(HttpMethod.POST, "/login").permitAll().anyRequest().authenticated())
-				.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
-				.exceptionHandling((exceptionHandling) -> exceptionHandling.authenticationEntryPoint(exceptionHandler));
-		return http.build();
-	}
-
-	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
-	public AuthenticationManager uthenticationManager(AuthenticationConfiguration authConfig) throws Exception {
-		return authConfig.getAuthenticationManager();
-	}
-
-	@Bean
-	public CorsConfigurationSource corsConfigurationSource() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowedOrigins(Arrays.asList("*"));
-		config.setAllowedMethods(Arrays.asList("*"));
-		config.setAllowedHeaders(Arrays.asList("*"));
-		config.setAllowCredentials(false);
-		config.applyPermitDefaultValues();
-		source.registerCorsConfiguration("/**", config);
-		return source;
 	}
 }
